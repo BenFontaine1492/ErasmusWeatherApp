f04783e8897f6f360bd7155b0af30ce6dc93a0e7
SmellyElld <60914@gymnasium.ax>
2025-05-08 10:25:46 +0300

    change where routing happens

--- src/Router.php
+++ src/Router.php
@@ -1,30 +1,6 @@
 <?php
 declare(strict_types = 1);
 
-
-$uri = new Uri;
-$router = new Router($routes);
-
-$routeObject = $router -> findRoute($uri -> getUrlArr(''));
-
-//Handle preflight requests
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    new Response([], 204);
-}
-
-if ($routeObject -> route) {
-    require_once './request.php';
-    require_once './db.php';
-
-    //include routed file
-    require_once './endpoints/' . $routeObject -> route -> file;
-    //Call routed function
-    call_user_func_array($routeObject -> route -> function, [['params' => $routeObject -> params, 'request' => new Request()]]);
-} else {
-    Response::notFound();
-}
-
-
 class Router {
 
     private array $routes = [];
--- src/index.php
+++ src/index.php
@@ -1,8 +1,29 @@
 <?php
-
 declare(strict_types=1);
 
 require_once("./response.php");
 require_once("./routes.php");
 require_once("./router.php");
+
+$uri = new Uri;
+$router = new Router($routes);
+
+$routeObject = $router -> findRoute($uri -> getUrlArr(''));
+
+//Handle preflight requests
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    new Response([], 204);
+}
+
+if ($routeObject -> route) {
+    require_once './request.php';
+    require_once './db.php';
+
+    //include routed file
+    require_once './endpoints/' . $routeObject -> route -> file;
+    //Call routed function
+    call_user_func_array($routeObject -> route -> function, [['params' => $routeObject -> params, 'request' => new Request()]]);
+} else {
+    Response::notFound();
+}
 ?>
