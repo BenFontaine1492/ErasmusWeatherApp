186a592ba01b561ee688277ce4780d86c89a17b4
Benjamin Ruff <benjamin.ruff@flyeralarm.com>
2025-05-12 12:26:52 +0200

    added warnings column to init.sql

--- docs/db/init.sql
+++ docs/db/init.sql
@@ -5,6 +5,7 @@ create table weather_data_fin (
     time datetime,
     pressure float,
     location varchar(100)
+    warnings text
 );
 create table weather_data_ger (
     id int auto_increment primary key,
@@ -13,4 +14,5 @@ create table weather_data_ger (
     time datetime,
     pressure float,
     location varchar(100)
+    warnings text
 );
